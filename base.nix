d9c8582ea3337946b22eab6a9fd5aa66886a4071
mjmena <martinjxm@gmail.com>
2024-01-25 08:25:39 -0500

    Initilize repo to work on tower(desktop)

--- base.nix
+++ base.nix
@@ -5,21 +5,13 @@
 { config, pkgs, ... }:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-    ];
+  imports = [];
+
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-
-  networking.hostName = "box"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Enable networking
   networking.networkmanager.enable = true;
@@ -61,16 +53,7 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.marty = {
@@ -134,7 +117,7 @@ environment.variables.EDITOR =  "nvim";
               gnome-contacts
               gnome-initial-setup
             ]);
-            programs.dconf.enable = true;
-	    system.stateVersion = "23.11"; # Did you read the comment?
+    programs.dconf.enable = true;
+    system.stateVersion = "23.11"; # Did you read the comment?
 
 }
