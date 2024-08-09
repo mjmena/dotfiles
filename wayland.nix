1e1ca8546e50d73620f0a735892b8d4b030bb89d
mjmena <martinjxm@gmail.com>
2024-08-09 18:05:14 -0400

    add tuigreet and testing programs for waybar

--- wayland.nix
+++ wayland.nix
@@ -1,7 +1,35 @@
 { pkgs, ... }: {
 
+  services.greetd = {
+    enable = true;
+    settings = {
+      default_session = {
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --cmd hyprland";
+        user = "marty";
+      };
+      initial_session = {
+        user = "marty";
+        command = "hyprland";
+      };
+    };
+  };
+
+  # this is a life saver.
+  # literally no documentation about this anywhere.
+  # might be good to write about this...
+  # https://www.reddit.com/r/NixOS/comments/u0cdpi/tuigreet_with_xmonad_how/
+  systemd.services.greetd.serviceConfig = {
+    Type = "idle";
+    StandardInput = "tty";
+    StandardOutput = "tty";
+    StandardError = "journal"; # Without this errors will spam on screen
+    # Without these bootlogs will spam on screen
+    TTYReset = true;
+    TTYVHangup = true;
+    TTYVTDisallocate = true;
+  };
   programs.hyprland.enable = true;
-  xdg.portal.enable = true;
+  # xdg.portal.enable = true;
 
   environment.systemPackages = [
     pkgs.waybar
@@ -10,6 +38,8 @@
     pkgs.swww
     pkgs.kitty
     pkgs.rofi-wayland
+    pkgs.wlogout
+    pkgs.pavucontrol
+    pkgs.spotify-player
   ];
-
 }
