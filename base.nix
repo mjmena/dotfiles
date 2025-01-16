3570b80a75616fd2dc51473657208b7e62223449
mjmena <martinjxm@gmail.com>
2025-01-16 04:57:50 -0500

    fix time for dual boot to windows

--- base.nix
+++ base.nix
@@ -55,6 +55,7 @@
 
   # Set your time zone.
   time.timeZone = "America/New_York";
+  time.hardwareClockInLocalTime = true;
 
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
