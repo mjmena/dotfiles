7a3b376f43dcdeb18cbc1b214215347c823e4492
mjmena <martinjxm@gmail.com>
2025-02-02 07:30:18 -0500

    feat: enable access to my server for hepaestus via sshkey

--- gaia/configuration.nix
+++ gaia/configuration.nix
@@ -9,6 +9,7 @@
     settings.PermitRootLogin = "no";
     knownHosts = {
       hermes.publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGCER5LsQDVarvt5/iASmnP6M1lU8mj4ZW4oZxTb8TiU";
+      hephaestus.publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPAGrfWIpQ/j/fx4KqmsxvKQY1VTKH1XJdOIw929/yhq";
     };
   };
 
