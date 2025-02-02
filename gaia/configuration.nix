be95a516d90c0602d7ef20a7a544331887461ee7
mjmena <martinjxm@gmail.com>
2025-02-02 04:42:02 -0500

    feat: enable access to my server for hermes via ssh key

--- gaia/configuration.nix
+++ gaia/configuration.nix
@@ -7,6 +7,9 @@
     enable = true;
     ports = [ 22 ];
     settings.PermitRootLogin = "no";
+    knownHosts = {
+      hermes.publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGCER5LsQDVarvt5/iASmnP6M1lU8mj4ZW4oZxTb8TiU";
+    };
   };
 
   stylix.fonts.sizes = {
