742b7e3f627ddbfaa2e658094e71054a5034d277
marty <martinjxm@gmail.com>
2025-12-15 21:31:12 -0500

    feat: Configure ACME and Nginx for mjmena.com with SSL and proxy to localhost:3000.

--- apheleia/configuration.nix
+++ apheleia/configuration.nix
@@ -12,4 +12,27 @@
       hephaestus.publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPAGrfWIpQ/j/fx4KqmsxvKQY1VTKH1XJdOIw929/yhq";
     };
   };
+
+  security.acme = {
+    acceptTerms = true;
+    defaults.email = "martinjxm@gmail.com";
+  };
+
+  networking.firewall.allowedTCPPorts = [
+    80
+    443
+  ];
+
+  services.nginx = {
+    enable = true;
+    recommendedProxySettings = true;
+    recommendedTlsSettings = true;
+    virtualHosts."mjmena.com" = {
+      enableACME = true;
+      forceSSL = true;
+      locations."/" = {
+        proxyPass = "http://localhost:3000";
+      };
+    };
+  };
 }
